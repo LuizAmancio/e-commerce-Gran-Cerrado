26b895d11f91c63141b5c58b432e11830bcf8ff6
LuizAmancio <luizfelipeamancio@gmail.com>
2025-10-03 12:10:27 -0300

    fix: Ajustes menu

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -8,22 +8,22 @@ export const MenuBar:FC<{ cartCount: number }> = ({ cartCount }) => {
     return (
       <header className="text-white sticky top-0 z-50 shadow-lg">
         <div className="max-w-7xl mx-auto px-4 py-3">
-          <div className="flex items-center justify-between">
-            <button className="lg:hidden">
+          <div className="flex items-center justify-between relative">
+            <button>
               <Menu size={28} />
             </button>
             
-            <div className="flex items-center gap-3">
+            <div className="flex items-center gap-3 bg-transparent  md:absolute md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
               <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center">
                 <span className="text-2xl">🌳</span>
               </div>
-              <div className='hidden md:block'>
-                <h1 className="text-md font-bold">GRAN CERRADO</h1>
+              <div className=''>
+                <h1 className="text-sm md:text-xl font-bold">GRAN CERRADO</h1>
                 <p className="text-xs text-green-100">Produtos Naturais</p>
               </div>
             </div>
 
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-1 md:gap-4">
                <button className="hover:bg-green-600 p-2 rounded-full transition cursor-pointer">
                  <User />
                </button>
